925f36dc1d6a2e56c2ae9656b3444ebde681c044
Sanaa <sanaa.abd19@yahoo.fr>
2025-01-29 14:37:58 +0100

    first

---
+++ src/main/java/fr/efrei/rag/domain/Document.java
@@ -0,0 +1,36 @@
+package fr.efrei.rag.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Document {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
